7ef656dc8ffcc5f0f19d43006446f2bb894737e3
King Irrisorie <102375022+kingIrrisorie@users.noreply.github.com>
2023-11-10 23:30:59 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Listagem de Jogos</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="estilos/estilo.css">
+    </head>
+<body>
+    <div id="corpo">
+        <h1>Escolha seu jogo</h1>
+        <table class="listagem">
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+        </table>
+    </div>
+</body>
+</html>
---
+++ modelo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Titulo da pagina</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="estilos/estilo.css">
+    </head>
+<body>
+    <div id="corpo">
+        <h1></h1>
+    </div>
+</body>
+</html>
