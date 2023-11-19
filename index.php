45dd562156595f67f9067d2eb5aa38d2d4e13aae
King Irrisorie <102375022+kingIrrisorie@users.noreply.github.com>
2023-11-19 15:27:33 -0300

    funcao para pegar imagem

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <body>
     <?php
         require_once "includes/banco.php";
+        require_once "includes/funcoes.php";
     ?>
     <div id="corpo">
         <h1>Escolha seu jogo</h1>
@@ -21,7 +22,8 @@
                         echo "<tr><td>Nenhum registro encontrado";
                     } else {
                         while ($reg = $busca->fetch_object()) {
-                            echo "<tr><td>$reg->capa<td>$reg->nome";
+                            $t = thumb($reg->capa);
+                            echo "<tr><td><img src='$t' class='mini'/><td>$reg->nome";
                             echo "<td>Adm";
                         }
                     }
